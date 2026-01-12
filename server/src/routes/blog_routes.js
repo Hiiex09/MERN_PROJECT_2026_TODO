7640e69c6961b71343c28fefe4728bace693b87e
devonbarangan <barangandevon@gmail.com>
2026-01-12 19:20:40 +0800

    chore: blog routes data

--- server/src/routes/blog_routes.js
+++ server/src/routes/blog_routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { generateBlog } from "../controller/blog_controller.js";
+import { generateBlog, getBlogs } from "../controller/blog_controller.js";
+import { protectRoute } from "../middlewares/auth_middlewares.js";
 
 const router = express.Router();
 
 router.post("/create", generateBlog);
+router.get("/:userId/blogs", protectRoute, getBlogs);
 
 export default router;
